52e4b3b8476c1f8e38da82df72895b86d6ab43c2
Sergei Shneider <shneider.s@gmail.com>
2020-08-22 13:23:32 -0400

    added direct page parsing

--- background.js
+++ background.js
@@ -3,8 +3,9 @@ chrome.runtime.onMessage.addListener(
      (request, sender, sendResponse) => {
        (async ()=>{
         let data = await fetchByName(request)
-        data = await fetchByIds(data, request.apiKey)
-       // console.log('finalData', data)
+        // data = await fetchByIds(data, request.apiKey)
+        data = await fetchByIds(data, request.player)
+       console.log('finalData', data)
         sendResponse({aliData:data})
        })()
        return true;
@@ -25,15 +26,14 @@ async function fetchByName(request){
     })
    return fetchedByName.json()
 }
-
-async function fetchByIds(aliObject, apiKey){
-  //console.log('ali', aliObject)
+async function fetchByIds(aliObject, playerName){
   const idsArray = [];
-  aliObject.players.forEach(player=>{
-    idsArray.push(player.id)
-  })
-  const idsToFetch = idsArray.join(';')
-  const fetchedByIds = await fetch(`http://aligulac.com//api/v1/player/set/${idsToFetch}/?apikey=${apiKey}`,
+    aliObject.players.forEach(player=>{
+      idsArray.push(player.id)
+    })
+    const resultsArray = []
+    idsArray.forEach(async (id) =>  {
+      const fetchedById = await fetch(`http://aligulac.com/players/${id}-${playerName}`,
   {
           'method':'GET',
           'headers':{
@@ -43,5 +43,34 @@ async function fetchByIds(aliObject, apiKey){
 
           }
   })
-  return fetchedByIds.json();
+  // //fetchedById.body.read().then
+  const reader = fetchedById.body.getReader()
+  let resOut
+  reader.read().then((result)=> {
+    resOut = new TextDecoder("utf-8").decode(result.value)
+    console.log(resOut)
+    resultsArray.push(resOut)
+  })
+  })
+    return resultsArray
 }
+//FETCH BY IDS BY API. DOESNT RETURN WIN/LOSS
+// async function fetchByIds(aliObject, apiKey){
+//   //console.log('ali', aliObject)
+//   const idsArray = [];
+//   aliObject.players.forEach(player=>{
+//     idsArray.push(player.id)
+//   })
+//   const idsToFetch = idsArray.join(';')
+//   const fetchedByIds = await fetch(`http://aligulac.com//api/v1/player/set/${idsToFetch}/?apikey=${apiKey}`,
+//   {
+//           'method':'GET',
+//           'headers':{
+//               "Access-Control-Allow-Origin":"*",
+//               'Access-Control-Allow-Methods': 'GET',
+//               'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+
+//           }
+//   })
+//   return fetchedByIds.json();
+// }
